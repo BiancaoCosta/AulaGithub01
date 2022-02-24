6b8c2b365a1512923abecaaa804126269ec3deab
Aluno IFSC <IFSC@CDR-691786.cacador.ifsc.edu.br>
2022-02-24 17:02:35 -0300

    Início de projeto - Imprimindo meu nome com a classe Scanner

---
+++ Github/src/github/Github.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author IFSC
+ */
+public class Github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+    
+            Scanner leia = new Scanner(System.in);
+            System.out.println ("Entre com seu nome");
+            String name = leia.nextLine ();
+            System.out.println(nome+ "bem vindo ao GitHub!");
+            
+    
+        // TODO code application logic here
+    
+    
+}
